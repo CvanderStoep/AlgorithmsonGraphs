280dceabc65ddf4665a44a1ee8e3015f0fd31178
carlo <carlo.vanderstoep@gmail.com>
2021-02-17 14:43:42 +0100

    finding an exit from the maze; initial commit

--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -1,7 +1,10 @@
+# Uses python3
+
 import sys
 
 
-def reach(adj, x, y):
+def number_of_components(adj):
+    result = 0  # this is the number of connected components (CC)
     number_vertices = len(adj)
     visited = [False] * number_vertices
 
@@ -11,25 +14,27 @@ def reach(adj, x, y):
             if not visited[neighbour]:
                 explore(neighbour)
 
-    explore(vertex=x)
-    # print(visited)
-    return visited[y]
+    for vertex in range(number_vertices):
+        # keep exploring connected vertices until it stops, then add +1 to the CC
+        if not visited[vertex]:
+            explore(vertex=vertex)
+            result += 1
+    return result
 
 
 def main():
     input_data = sys.stdin.readline().strip()
-
+    # reads in the data as a single string separated with spaces
+    # number vertices, number of edges, edge_1, edge_2, ... edge_n
     data = list(map(int, input_data.split()))
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    x, y = data[2 * m:]
     adj = [[] for _ in range(n)]
-    x, y = x - 1, y - 1
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+    print(number_of_components(adj))
 
 
 if __name__ == '__main__':
